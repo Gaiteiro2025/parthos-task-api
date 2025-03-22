f0ad9dfda5e553e19b8ee50876f2dc62c2c7c1cf
Gaiteiro2025 <allansgaiteiro+dev@gmail.com>
2025-03-22 16:05:15 -0300

    atualizando o app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,17 @@ import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { appDataSource } from './ormconfig';
-import { UsersModule } from './users/users.module';
+import { TaskHistoryModule } from './task-history/task-history.module';
+import { TaskMessageModule } from './task-message/task-message.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(appDataSource.options),
     AuthModule,
-    UsersModule,
+    TaskModule,
+    TaskHistoryModule,
+    TaskMessageModule
   ],
   providers: [ConfigService],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,12 +2,10 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { UsersModule } from '../users/users.module';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
-    UsersModule,
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'default_secret',
